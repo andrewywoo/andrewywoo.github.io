90c65a071b2affdc48395da35a521fe2c5b687e1
Andrew Woo <andrew.woo@daily-harvest.com>
2020-06-22 19:56:37 -0400

    feat: initial commit for dev portfolio

---
+++ src/components/Typewriter/index.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types"
+import React from "react"
+import "./Typewriter.css"
+
+const Typewriter = ({ speed }) => {
+  return (
+    <div class="typewriter">
+      <h1>I am a software developer.</h1>
+    </div>
+  )
+}
+
+Typewriter.propTypes = {
+  speed: PropTypes.number,
+}
+
+Typewriter.defaultProps = {
+  speed: 3.5,
+}
+
+export default Typewriter
