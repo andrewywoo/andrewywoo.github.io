4d46a1fe4e73a5bd2333d0f86c77dd3cc986d802
Andrew Woo <andrew.woo@daily-harvest.com>
2020-07-03 09:08:02 -0400

    feat: adding google analytics to track site visitor data to gain insights' ' '

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,52 @@
 module.exports = {
-  siteMetadata: {
-    title: `Andrew Woo`,
-    description: `Andrew Woo Software Engineer Site`,
-    author: `@andrewywoo`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Andrew Woo`,
+        description: `Andrew Woo Software Engineer Site`,
+        author: `@andrewywoo`,
     },
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts`,
-      },
-    },
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `andrew-woo-dev-portfolio`,
-        short_name: `aw-portfolio`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-emotion`,
-    {
-      resolve: `gatsby-plugin-google-adsense`,
-      options: {
-        publisherId: `ca-pub-1431127993178422`
-      },
-    },
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `posts`,
+                path: `${__dirname}/src/posts`,
+            },
+        },
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `andrew-woo-dev-portfolio`,
+                short_name: `aw-portfolio`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/favicon.png`,
+            },
+        },
+        `gatsby-plugin-emotion`,
+        {
+            resolve: `gatsby-plugin-google-adsense`,
+            options: {
+                publisherId: `ca-pub-1431127993178422`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-171543313-1",
+            },
+        },
+    ],
 }
